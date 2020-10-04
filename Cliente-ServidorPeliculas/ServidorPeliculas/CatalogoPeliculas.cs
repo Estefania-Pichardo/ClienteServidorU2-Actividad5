452d7640cad9363b8c4a8ca59d893cee7d05344e
Estefania-Pichardo <70533422+Estefania-Pichardo@users.noreply.github.com>
2020-10-04 13:44:02 -0500

    ServidorPeliculas

---
+++ Cliente-ServidorPeliculas/ServidorPeliculas/CatalogoPeliculas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ServidorPeliculas
+{
+    public class CatalogoPeliculas
+    {
+        public ObservableCollection<Pelicula> ListaPeliculas { get; set; } = new ObservableCollection<Pelicula>();
+
+        public void Agregar(Pelicula p)
+        {
+            ListaPeliculas.Add(p);
+            Guardar();
+        }
+        public void Editar(Pelicula p)
+        {
+            var pelicula = ListaPeliculas.FirstOrDefault(x => x.Nombre == p.Nombre & x.Hora==p.Hora);
+
+            if(pelicula!=null)
+            {
+                pelicula.Idioma = p.Idioma;
+                pelicula.Sala = p.Sala;
+                Guardar();
+            }
+        }
+        public void Eliminar(Pelicula p)
+        {
+            var pelicula = ListaPeliculas.FirstOrDefault(x => x.Nombre == p.Nombre & x.Hora == p.Hora);
+            if(pelicula!=null)
+            {
+                ListaPeliculas.Remove(pelicula);
+                Guardar();
+            }
+        }
+
+        private void Guardar()
+        {
+            string datos = JsonConvert.SerializeObject(ListaPeliculas);
+            File.WriteAllText("peliculas.json", datos);
+        }
+        private void Cargar()
+        {
+            if (File.Exists("peliculas.json"))
+            {
+                var datos = JsonConvert.DeserializeObject<ObservableCollection<Pelicula>>(File.ReadAllText("peliculas.json"));
+
+                foreach (var item in datos)
+                {
+                    ListaPeliculas.Add(item);
+                }
+            }
+        }
+        public CatalogoPeliculas()
+        {
+            Cargar();
+        }
+    }
+}
---
+++ Cliente-ServidorPeliculas/ServidorPeliculas/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ServidorPeliculas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PeliculasServer server = new PeliculasServer();
+
+        private int time=0;
+        private DispatcherTimer Timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = server;
+            dtgPeliculas.ItemsSource = server.CatalogoPeliculas.ListaPeliculas;
+
+            Timer = new DispatcherTimer();
+            Timer.Interval = new TimeSpan(0, 0, 10);
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(time>=0)
+            {
+                dtgPeliculas.ItemsSource = server.CatalogoPeliculas.ListaPeliculas;
+                time++;
+            }
+        }
+    }
+}
